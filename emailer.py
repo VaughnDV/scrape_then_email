05003d5a5bae29a4c129a7f8323aeea5ab4602dc
Vaughn de Villiers <vaughndevilliers@gmail.com>
2017-05-20 07:35:02 +0100

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 
 # me == my email address
 # you == recipient's email address
-me = "reportprowlers@gmail.com"
+me = "your.email@provider.com"
 
 with open('saidsacontacts.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
@@ -15,7 +15,7 @@ with open('saidsacontacts.csv') as csvfile:
 		print row['EMAIL']
 		# Create message container - the correct MIME type is multipart/alternative.
 		msg = MIMEMultipart('alternative')
-		msg['Subject'] = "Report Prowlers Service"
+		msg['Subject'] = "YOUR COMPANY SERVICE"
 		msg['From'] = me
 		msg['To'] = row['EMAIL']
 
@@ -26,7 +26,7 @@ with open('saidsacontacts.csv') as csvfile:
 		  <head>
 		    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		    <meta name="viewport" content="width=device-width">
-		    <title>Report Prowlers</title>
+		    <title>YOUR COMPANY</title>
 		    <style>
 		    body {
 				  width: 100% !important;
@@ -45,56 +45,22 @@ with open('saidsacontacts.csv') as csvfile:
 
 
 
-		    <img src="http://www.reportprowlers.co.za/static/images/reportprowlersbanner.png">
+		    <img src="LINK TO SOME IMAGE">
 
 
 			  <p>Dear Sirs, <br></p>
 			  <p>Please may I have a moment of your time to explain our service.<br></p>
-			  <p>Report Prowlers is an internet based service designed to advance security in neighborhoods through simplified online communication between the neighborhood's residents, and the local security services.<br><br></p>
-			  <h3 class="text-center" style="color: #28475C;"><strong>How it works:</strong></h3>
-			  <p><br><strong>USERS = Area's Residents and Security Personal on patrol</strong><br><br>
-			  Users login, and send reports of suspicious activity in their area. Its free, and easy to use. From the USERS perspective, the process is as follows:<br><br>
-			  </p>
-			  <p class="text-center" style="color: #28475C;">Login > Send Report > Safer Neighborhood</p>
-			  <br><br>
-			  </p>
-			  <p><strong>SUBSCRIBERS = Area's Security Services: </strong><br><br>
-			  Receive these reports, with additional intelligence on comparisons with other recent reports, recommended response level. Subscribers pay a monthly subscription fee for each area they wish to monitor:<br><br></p>
-			  <p class="text-center" style="color: #28475C;">Monitor Emails > Receive Reports > Allocate Appropriate Response</p>
-			  <br><br>
-			  <p><strong>BUILT TO BE:</strong><br><br>
-			  <ul>
-			    <li>Fast and reliable, using Google's Emailing Servers</li>
-			    <li>Easy to use, and mobile phone friendly</li>
-			    <li>Stable using well established technologies</li>
-			    <li>Free for Users to send reports</li>
-			    <li>Cost effective subscription fees at R150/Month per subscribed area</li>
-			    <li>Helpful in your marketing stratergy to grow your client base, or promote your services</li>
-			    <li>Promoted on social media platforms such as facebook and twitter ** Campaigns already under way</li>
-			    <li>Easy to setup: No installation, no additional hardware, no proprietary software</li>
-			    <li>A proactive approach to being at the right place at the right time</li>
-			    <li>Helpful in deciding where to allocate resources</li>
-			    <li>Structured and easy to understand reports</li>
-			    <li>Helpful in building intelligence</li>
-			    <li>Available everywhere in South Africa</li>
-			  </ul>
-			  <br><br></p>
-			  <p class="text-center" style="color: #28475C;">Click on the image below to visit the website</p>
-
-			  <a href="http://www.reportprowlers.co.za"> <img src="http://www.reportprowlers.co.za/static/images/fbreportprowlers.png"></a>
 			  
-			  <h3>Click <a href="www.reportprowlers.co.za/register"> here </a>to subscribe or to recieve additional information</h3>
-
-			  <p>Please do not hesitate to <a href="mailto:reportprowlers@gmail.com">contact us</a> should you have any queries</p>
+			  <p>Please do not hesitate to <a href="mailto:your.email@provider.com">contact us</a> should you have any queries</p>
 
 			  <p>Sincerely Yours,<br><br></p>
 
-		    <h5>Hannah Swan</h5>
+		    <h5>SALES CONSULTANT</h5>
 		    <p>Sales Manager<br>
-		    Report Prowlers<br>
-		    Phone: 039 319 1088<br>
-		    Mobile: 078 738 4038<br>
-		    Email: <a href="mailto:reportprowlers@gmail.com">reportprowlers@gmail.com</a></p>
+		    company name<br>
+		    Phone: 555 319 1088<br>
+		    Mobile: 555 738 4038<br>
+		    Email: <a href="mailto:your.email@provider.com">your.email@provider.com</a></p>
 
 
 		  </body>
@@ -115,8 +81,8 @@ with open('saidsacontacts.csv') as csvfile:
 
 
 
-		gmail_user = 'reportprowlers@gmail.com'
-		gmail_password = '123password123'
+		gmail_user = 'your.email@provider.com'
+		gmail_password = 'yourpassword'
 
 		try:
 			#server = smtplib.SMTP('smtp.gmail.com:587')
